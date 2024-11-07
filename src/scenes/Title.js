2e0e995ecf7943bcaf1442055bfbc1a693fb0df2
firekicks <114191519+firekicks@users.noreply.github.com>
2024-11-06 17:20:23 -0800

    pushing background image

--- src/scenes/Title.js
+++ src/scenes/Title.js
@@ -6,9 +6,17 @@ class Title extends Phaser.Scene {
     preload() {
         this.load.setPath("./assets/");
         this.load.bitmapFont("pixel_square", "fonts/pixel_square_0.png", "fonts/pixel_square.fnt");
+
+        // Load your background image
+        this.load.image('background', './Photos/background.png'); // Adjust the path if needed
     }
 
     create() {
+        // Add the background image to the center of the canvas
+        this.add.image(game.config.width / 2, game.config.height / 2, 'background')
+            .setOrigin(0.5, 0.5)
+            .setDisplaySize(game.config.width, game.config.height);
+
         this.startGame = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.ENTER);
 
         this.add.bitmapText(game.config.width / 2, (game.config.height / 2) - 40, "pixel_square",
